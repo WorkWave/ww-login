26aa2f55d057556264079aafef0083cf5cdf1d86
Mike Lorzing <48525012+michaellorzing@users.noreply.github.com>
2022-02-03 14:20:31 -0500

    styling

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -9,17 +9,23 @@ import CssBaseline from "@mui/material/CssBaseline"
 import theme from "../theme"
 
 const Layout = ({ children, location }) => {
-  const { width, height } = queryString.parse(location.search)
-  console.log(width, height)
   return (
     <ThemeProvider theme={theme}>
-      <div style={{ maxHeight: "100vh" }}>
-        <CssBaseline />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <Container fixed sx={{ height: "100vh", overflow: "hidden" }}>
-          <main>{children}</main>
-        </Container>
-      </div>
+      <CssBaseline />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+      <main>
+        <Container
+          fixed
+          sx={{
+            maxHeight: `${location?.search?.height}px`,
+            maxWidth: `${location?.search?.width}px`,
+            overflow: "hidden",
+          }}
+        >
+          {children}
+        </Container>{" "}
+      </main>
     </ThemeProvider>
   )
 }
