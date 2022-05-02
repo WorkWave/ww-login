ff627de5b95125cf6c3ed7d0c8c8e2592f8a6224
Mike Lorzing <48525012+michaellorzing@users.noreply.github.com>
2022-05-02 09:38:01 -0400

    fix gif

--- web/src/components/content/CarouselSlides/Image.js
+++ web/src/components/content/CarouselSlides/Image.js
@@ -3,11 +3,10 @@ import React from "react"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 //gatsby image can't work with svgs. ternary allows rendering of any image type
+// 5/2 update with gif. needs to be fixed with ternary added
 
 export const Image = ({ contentImage }) => {
-  return contentImage.asset.gatsbyImageData.images?.sources.length ? (
-    <img src={contentImage.asset.url} alt="chart" />
-  ) : (
+  return (
     <GatsbyImage
       image={contentImage?.asset?.gatsbyImageData}
       alt="chart"
